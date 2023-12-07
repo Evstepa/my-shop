21b44f0b5b73faa9f103bc8be40175ce88fc2134
Елена Степаненко <stepanenko.ei@yandex.ru>
2023-12-07 21:29:54 +0300

    Пагинация и фильтры. Начало

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -14,35 +14,16 @@
       {{ product.price }} ₽
     </span>
 
-    <ul class="colors colors--black">
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-          <span class="colors__value" style="background-color: #73B6EA;">
-          </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-          <span class="colors__value" style="background-color: #8BE000;">
-          </span>
-        </label>
-      </li>
-      <li class="colors__item">
-        <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-          <span class="colors__value" style="background-color: #222;">
-          </span>
-        </label>
-      </li>
-    </ul>
+    <ProductColors :colorsId="product.colorsId" />
     
   </div>
 </template>
 
 <script>
+  import ProductColors from './ProductColors';
+
   export default {
+    components: { ProductColors },
     name: 'ProductItem',
     props: {
       product: {
